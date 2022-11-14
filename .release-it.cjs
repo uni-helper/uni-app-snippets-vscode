744a4fe8667b6e5aedface28591dc8ee15855046
ModyQyW <wurui-dev@foxmail.com>
2022-11-14 16:46:11 +0800

    style: format

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,13 +1,14 @@
 module.exports = {
-  "git": {
-    "commitMessage": "chore(release): v${version}",
-    "tagName": "v${version}"
+  git: {
+    commitMessage: 'chore(release): v${version}',
+    tagName: 'v${version}',
   },
-  "npm": {
-    "publish": false,
-    "timeout": 30
+  npm: {
+    publish: false,
+    timeout: 30,
   },
-  "hooks": {
-    "after:bump": "tsx ./scripts/generate.ts && vsce package && vsce publish -p $VSCE_PAT && ovsx publish -p $OVSX_PAT"
-  }
-}
+  hooks: {
+    'after:bump':
+      'tsx ./scripts/generate.ts && vsce package && vsce publish -p $VSCE_PAT && ovsx publish -p $OVSX_PAT',
+  },
+};
