40e784d49a0588f3032534b1295a5d62ed60de93
ModyQyW <wurui-dev@foxmail.com>
2022-11-15 08:12:43 +0800

    chore: update release-it hooks

--- .release-it.cjs
+++ .release-it.cjs
@@ -8,7 +8,6 @@ module.exports = {
     timeout: 30,
   },
   hooks: {
-    'after:bump':
-      'tsx ./scripts/generate.ts && vsce package --no-dependencies && vsce publish -p $VSCE_PAT --no-dependencies && ovsx publish -p $OVSX_PAT',
+    'after:bump': 'tsx ./scripts/generate.ts',
   },
 };
